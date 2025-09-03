ac7bf01d57a14bb1ba69dee393dce17e352bedb3
Artem <tem4ik20.1@gmail.com>
2025-09-03 20:06:49 +0300

    fix close submit

--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -18,6 +18,7 @@ export default function NoteForm() {
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["notes"] });
       clearDraft();
+      handleCancel();
     
     },
   });
